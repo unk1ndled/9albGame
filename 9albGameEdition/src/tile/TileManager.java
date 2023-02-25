2ed04b189b1a585972b367337501d36b887fd2fb
Bisho11 <hamza.bichou@live.fr>
2023-02-25 20:44:13 +0100

    final camera mechanism

--- 9albGameEdition/src/tile/TileManager.java
+++ 9albGameEdition/src/tile/TileManager.java
@@ -91,8 +91,15 @@ public class TileManager {
 			int screenX = worldX - gp.player.worldX + gp.player.screenX;
 			int screenY = worldY - gp.player.worldY + gp.player.screenY;
 			
+			if( worldX + gp.tileSize > gp.player.worldX -gp.player.screenX &&
+				worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+				worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+				worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+				
+				g2.drawImage(tile[tileNum].image,screenX,screenY,gp.tileSize,gp.tileSize,null);
+			}
 			
-			g2.drawImage(tile[tileNum].image,screenX,screenY,gp.tileSize,gp.tileSize,null);
+			
 			worldCol++;
 
 			
