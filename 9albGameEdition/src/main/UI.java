fef44c0a0355b8e2039e041d87c77ee63f6c5d08
Bisho11 <hamza.bichou@live.fr>
2023-03-11 00:57:39 +0100

    added UI

--- 9albGameEdition/src/main/UI.java
+++ 9albGameEdition/src/main/UI.java
@@ -3,11 +3,18 @@ package main;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.text.DecimalFormat;
 
 public class UI {
 
 	GamePannel gp;
 	Font arial_40;
+	public boolean messageOn = false;
+	public String message = "";
+	int messageCounter = 0;
+	
+	double playTime;
+	DecimalFormat dFormat = new DecimalFormat("#0.0");
 	
 	public UI(GamePannel gp) {
 		this.gp = gp;
@@ -15,11 +22,32 @@ public class UI {
 		
 	}
 	
+	public void showMessage(String text) {
+		message = text;
+		messageOn = true;
+	}
+	
 	public void draw (Graphics2D g2) {
 		
 		g2.setFont(arial_40);
 		g2.setColor(Color.white);
 		g2.drawString("Imposters caught : ", 50, 50);
+		
+		//TIME
+		playTime +=(double)1/60;
+		g2.drawString("Time : "+dFormat.format(playTime),gp.tileSize , 65);
+		
+		//MESSAGE
+		if(messageOn == true) {
+			g2.drawString(message, 0, 0);
+			
+			messageCounter++;
+			
+			if(messageCounter > 120) {
+				messageCounter = 0;
+				messageOn = false;
+			}
+		}
 	}
 	
 	
